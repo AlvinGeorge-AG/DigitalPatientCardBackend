9d7ca9f8e2f3cf763d5dd996d5ad2f8a237bac9c
Ewanjohndennis <ewandennis2006@gmail.com>
2025-10-02 21:16:59 +0530

    Revert "PDF"
    This reverts commit d3d3b360fe7ac9c4de16c5622068d3e9a4167a34.

--- src/main/java/com/DPC/DigitalPatientCardBackend/repository/PatientRepository.java
+++ src/main/java/com/DPC/DigitalPatientCardBackend/repository/PatientRepository.java
@@ -1,13 +1,14 @@
 package com.DPC.DigitalPatientCardBackend.repository;
 
+
 import com.DPC.DigitalPatientCardBackend.patient.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
     Patient findByUsername(String username);
     void deletePatientByUsername(String username);
-    Patient findPatientById(Long id);
 
-    // Add this for PDF service
-    Patient findByPhoneNumber(String phoneNumber);
+    Patient findPatientById(Long id);
+    // You get save(), findAll(), findById(), deleteById(), etc. for free
 }
+
